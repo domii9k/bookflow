bc5ded6115b421f5c7a0c11e3171e0669316f443
Domi <110205632+domii9k@users.noreply.github.com>
2024-06-17 18:48:46 -0300

    Nova classe Configuration para Habilitar o CORS

---
+++ src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package edu.api.bookflow.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("http://192.168.10.112:3000")
+        .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
