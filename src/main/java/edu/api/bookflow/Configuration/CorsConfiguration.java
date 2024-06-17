2dc79cba3da741496cc5936276c92f4e295dbe6d
Domi <110205632+domii9k@users.noreply.github.com>
2024-06-17 18:55:59 -0300

    alterado o ip contido na classe CorsConfiguration

--- src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
+++ src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://192.168.10.112:3000")
+        .allowedOrigins("http://127.0.0.1:3000")
         .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
