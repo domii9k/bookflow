cf13f31086691f99ac6fae1e31b0ebc9f2816425
Dominique Ferreira <siqdominique99@gmail.com>
2024-06-17 22:23:37 -0300

    Desabilitando classe antiga de configuração CORS

--- src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
+++ src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
@@ -5,14 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
     
-    @Override
+   /* @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000")
         .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PUT");
-    }
+    }*/
 }
