dc4d927646c181aac1b78fd740958b1936526684
Dominique Ferreira <siqdominique99@gmail.com>
2024-06-18 21:14:38 -0300

    modificando permissões de origens CORS

--- src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
+++ src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("https://bookflow-phi.vercel.app/emprestimos")
+        .allowedOrigins("https://bookflow-phi.vercel.app/*")
         .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PUT");
     }
 }
