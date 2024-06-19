3cc851e62bb0e6661cf7e47c347010f542408df2
Dominique Ferreira <siqdominique99@gmail.com>
2024-06-18 21:01:35 -0300

    adicionando url do front no CORS

--- src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
+++ src/main/java/edu/api/bookflow/Configuration/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("https://bookflow-phi.vercel.app/emprestimos")
         .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PUT");
     }
 }
