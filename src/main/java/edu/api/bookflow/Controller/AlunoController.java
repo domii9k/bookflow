d808fa879572006ef425b0633361d91e402cc64c
Dominique Ferreira <siqdominique99@gmail.com>
2024-06-21 15:57:28 -0300

    aluno Controller

--- src/main/java/edu/api/bookflow/Controller/AlunoController.java
+++ src/main/java/edu/api/bookflow/Controller/AlunoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
