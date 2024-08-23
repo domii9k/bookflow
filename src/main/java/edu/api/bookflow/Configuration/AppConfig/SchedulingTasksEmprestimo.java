4128411f8e653fdb2dfedd9683bd81088b4c835d
Dominique Ferreira <siqdominique99@gmail.com>
2024-08-23 14:21:19 -0300

    alterando organização do pacote Configuration

---
+++ src/main/java/edu/api/bookflow/Configuration/AppConfig/SchedulingTasksEmprestimo.java
@@ -0,0 +1,11 @@
+package edu.api.bookflow.Configuration.AppConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingTasksEmprestimo {
+
+
+}
--- src/main/java/edu/api/bookflow/Configuration/WebConfig/CorsConfiguration.java
+++ src/main/java/edu/api/bookflow/Configuration/WebConfig/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package edu.api.bookflow.Configuration;
+package edu.api.bookflow.Configuration.WebConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
