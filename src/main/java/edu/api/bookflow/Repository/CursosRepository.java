4be309a69a7177c551ae913b854a1b4bf0d7241b
Dominique Ferreira <siqdominique99@gmail.com>
2024-08-23 14:39:28 -0300

    testes

--- src/main/java/edu/api/bookflow/Repository/CursosRepository.java
+++ src/main/java/edu/api/bookflow/Repository/CursosRepository.java
@@ -1,10 +1,10 @@
 package edu.api.bookflow.Repository;
 
 import org.springframework.stereotype.Repository;
-import edu.api.bookflow.Model.Cursos;
+import edu.api.bookflow.Model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CursosRepository extends JpaRepository<Cursos, Long>{
+public interface CursosRepository extends JpaRepository<Curso, Long>{
     
 }
