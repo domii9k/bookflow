d1c56c054dfdb907ef6a63eaaebd77b71874c250
Dominique Ferreira <siqdominique99@gmail.com>
2024-09-02 17:02:23 -0300

    adicionando novos recursos a service

--- src/main/java/edu/api/bookflow/Services/EmprestimoService.java
+++ src/main/java/edu/api/bookflow/Services/EmprestimoService.java
@@ -1,21 +1,20 @@
 package edu.api.bookflow.Services;
 
-import edu.api.bookflow.DTO.AlunoDTO;
 import edu.api.bookflow.DTO.EmprestimoDTO;
 import edu.api.bookflow.DTO.Mapper.EmprestimoMapper;
 import edu.api.bookflow.DTO.Pagination.PaginationDTO;
 import edu.api.bookflow.Exceptions.NotFoundObject;
-import edu.api.bookflow.Model.Aluno;
 import edu.api.bookflow.Model.Emprestimo;
 import edu.api.bookflow.Repository.EmprestimoRepository;
+import edu.api.bookflow.Services.patchHttpRequest.GlobalPatch;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
@@ -29,63 +28,130 @@ public class EmprestimoService {
     private final EmprestimoRepository emprestimoRepository;
     private final EmprestimoMapper emprestimoMapper;
     @Autowired
-    private final LivroService livroService;
-    @Autowired
-    private final AlunoService alunoService;
+    private GlobalPatch patcher;
 
-    public EmprestimoService(EmprestimoRepository emprestimoRepository, EmprestimoMapper emprestimoMapper, LivroService livroService, AlunoService alunoService) {
+    public EmprestimoService(EmprestimoRepository emprestimoRepository, EmprestimoMapper emprestimoMapper) {
         this.emprestimoRepository = emprestimoRepository;
         this.emprestimoMapper = emprestimoMapper;
-        this.livroService = livroService;
-        this.alunoService = alunoService;
     }
 
-    public PaginationDTO<EmprestimoDTO> listAll (@RequestParam(name = "pag")@PositiveOrZero int pageNumber,
-                                              @RequestParam(name = "size") @Positive @Max(50) int pageSize,
-                                              @RequestParam(value = "sortBy", defaultValue = "data_emp") String sortBy,
-                                              @RequestParam(value = "sortDir", defaultValue = "desc") String sortDir)  {
-        // Verifica se o valor de
+    /*public PaginationDTO<EmprestimoDTO> listAll(@RequestParam(name = "pag") @PositiveOrZero int pageNumber,
+                                                @RequestParam(name = "size") @Positive @Max(50) int pageSize,
+                                                @RequestParam(value = "sortBy", defaultValue = "data_emp") String sortBy,
+                                                @RequestParam(value = "sortDir", defaultValue = "desc") String sortDir) {
+
         Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Page<Emprestimo> page = emprestimoRepository.findAll(PageRequest.of(pageNumber, pageSize, sort));
         List<EmprestimoDTO> list = page.stream().map(emprestimoMapper::convertToDto).toList();
 
-        return  new PaginationDTO<>(list, page.getTotalElements(), page.getTotalPages());
+        return new PaginationDTO<>(list, page.getTotalElements(), page.getTotalPages());
+    }*/
+
+    public PaginationDTO<EmprestimoDTO> listAll(@RequestParam(name = "pag",defaultValue = "0") @PositiveOrZero int pageNumber,
+                                                @RequestParam(name = "size",defaultValue = "10") @Positive @Max(50) int pageSize,
+                                                @RequestParam(value = "sortBy", defaultValue = "dataEmprestimo") String sortBy,
+                                                @RequestParam(value = "sortDir", defaultValue = "desc") String sortDir,
+                                                 @RequestParam(name = "cancelado", defaultValue = "false") Boolean cancelado,
+                                                 @RequestParam(name = "foiDevolvido", defaultValue = "false") Boolean foiDevolvido,
+                                                 @RequestParam(name = "atrasado", defaultValue = "false") Boolean atrasado) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Page<Emprestimo> page = emprestimoRepository.findAllByCanceladoAndAtrasado(cancelado, foiDevolvido, atrasado, PageRequest.of(pageNumber, pageSize, sort));
+        List<EmprestimoDTO> list = page.stream().map(emprestimoMapper::convertToDto).toList();
+
+        return new PaginationDTO<>(list, page.getTotalElements(), page.getTotalPages());
     }
 
-    public EmprestimoDTO findById(@Positive @NotNull Long id){
+    public EmprestimoDTO findById(@Positive @NotNull Long id) {
         return emprestimoRepository.findById(id).map(emprestimoMapper::convertToDto).orElseThrow(() -> new NotFoundObject(id));
     }
 
-    public EmprestimoDTO create(@Valid EmprestimoDTO dto){
+    public EmprestimoDTO create(@Valid EmprestimoDTO dto) {
         validaAlunoOnCreateEmprestimo(dto);
-        livroService.validaLivroOnCreateEmprestimo(dto);
+        validaLivroOnCreateEmprestimo(dto);
         return emprestimoMapper.convertToDto(emprestimoRepository.save(emprestimoMapper.convertToEntity(dto)));
     }
 
-    private void validaAlunoOnCreateEmprestimo(EmprestimoDTO emprestimoDTO){
+    public EmprestimoDTO update(@Positive Long id, @Valid EmprestimoDTO dto) {
+        return emprestimoRepository.findById(id)
+                .map(registroEncontrado -> {
+                    BeanUtils.copyProperties(dto, registroEncontrado);
+                    return emprestimoMapper.convertToDto(emprestimoRepository.save(registroEncontrado));
+                }).orElseThrow(() -> new NotFoundObject(id));
+    }
+
+    public EmprestimoDTO patch(@Positive Long id, @Valid EmprestimoDTO emprestimoIncompleto) {
+        Emprestimo emprestimoExistente = emprestimoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundObject(id));
+        try {
+            patcher.patch(emprestimoExistente, emprestimoMapper.convertToEntity(emprestimoIncompleto));
+            emprestimoRepository.save(emprestimoExistente);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao aplicar o patch no empréstimo.", e);
+        }
+        return emprestimoMapper.convertToDto(emprestimoExistente);
+    }
+
+    public void delete(@Positive Long id) {
+        emprestimoRepository.delete(emprestimoRepository.findById(id).orElseThrow(() -> new NotFoundObject(id)));
+    }
+
+
+    /*
+     * Metodo utilizado para validar se o aluno já possui um empréstimo ativo
+     * @param EmprestimoDTO emprestimoDTO
+     * */
+    private void validaAlunoOnCreateEmprestimo(EmprestimoDTO emprestimoDTO) {
         Long idAluno = emprestimoDTO.codAluno().getCodAluno();
         LocalDate hoje = LocalDate.now();
-        List<Emprestimo> emprestimoList = emprestimoRepository.findAll();
 
-        for(Emprestimo emprestimo : emprestimoList){
-            if(emprestimo.getCodAluno().getCodAluno().equals(idAluno) && emprestimo.getDataPrevistaDevolucao().isAfter(hoje)){
-                throw new IllegalStateException("O aluno de id "+idAluno+" já possui um empréstimo ativo! Data de entrega prevista para "+emprestimo.getDataPrevistaDevolucao()+".");
+        // Busca todos os empréstimos do aluno no banco de dados
+        List<Emprestimo> emprestimosDoAluno = emprestimoRepository.findByCodAluno_CodAluno(idAluno);
+
+        for (Emprestimo emprestimo : emprestimosDoAluno) {
+            // Verifica se o empréstimo não foi devolvido
+            if (emprestimo.getFoiDevolvido() == false) {
+                // Verifica se o empréstimo está atrasado
+                if (emprestimo.getDataPrevistaDevolucao().isBefore(hoje)) {
+                    throw new IllegalStateException("O(A) aluno(a) " + emprestimo.getCodAluno().getNomeCompleto() + " possui um empréstimo atrasado! Data de entrega estava prevista para " + emprestimo.getDataPrevistaDevolucao() + ".");
+                } else {
+                    throw new IllegalStateException("O(A) aluno(a) " + emprestimo.getCodAluno().getNomeCompleto() + " já possui um empréstimo ativo com data de entrega prevista para " + emprestimo.getDataPrevistaDevolucao() + ".");
+                }
             }
         }
-
     }
 
     /*
-    * Método programado para ser executado todo dia à meia noite
-    *
-    * */
+     * Metodo utilizado para validar se o livro já está em um emréstimo ativo
+     * @param EmprestimoDTO emprestimoDTO
+     * */
+    private void validaLivroOnCreateEmprestimo(EmprestimoDTO emprestimoDTO) {
+        Long idLivro = emprestimoDTO.codLivro().getCodLivro();
+        LocalDate hoje = LocalDate.now();
+        List<Emprestimo> emprestimosDoLivro = emprestimoRepository.findByCodLivro_CodLivro(idLivro);
+
+        for (Emprestimo emprestimo : emprestimosDoLivro) {
+            // Verifica se o empréstimo está atrasado
+            if (emprestimo.getFoiDevolvido() == false) {
+                if (emprestimo.getDataPrevistaDevolucao().isBefore(hoje)) {
+                    throw new IllegalStateException("O livro de ID " + idLivro + " está vinculado a outro epréstimo em atraso! Data de entrega estava prevista para " + emprestimo.getDataPrevistaDevolucao() + ".");
+                } else {
+                    throw new IllegalStateException("O livro de ID " + idLivro + " já está emprestado! Data de entrega prevista para " + emprestimo.getDataPrevistaDevolucao() + ".");
+                }
+            }
+        }
+    }
+
+    /*
+     * Metodo programado para ser executado todo dia à meia noite
+     * */
     @Scheduled(cron = "0 0 0 * * *")
-    public void validaAtraso(){
+    public void validaAtraso() {
         LocalDate hoje = LocalDate.now();
         List<Emprestimo> emprestimoList = emprestimoRepository.findAll();
 
-        for(Emprestimo emprestimo : emprestimoList){
-            if (emprestimo.getDataPrevistaDevolucao().isBefore(hoje) && !emprestimo.isFoiDevolvido()){
+        for (Emprestimo emprestimo : emprestimoList) {
+            if (emprestimo.getDataPrevistaDevolucao().isBefore(hoje) && emprestimo.getFoiDevolvido() == false) {
                 emprestimo.setAtrasado(true);
             }
             emprestimoRepository.save(emprestimo);
