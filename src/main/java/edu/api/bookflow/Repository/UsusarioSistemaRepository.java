d6be711347685390a46d9e168c9ee4c1133395bb
Dominique Ferreira <siqdominique99@gmail.com>
2024-09-17 10:10:43 -0300

    adicionando uma query propria para retorno do usuario, com o status como parametro

--- src/main/java/edu/api/bookflow/Repository/UsusarioSistemaRepository.java
+++ src/main/java/edu/api/bookflow/Repository/UsusarioSistemaRepository.java
@@ -1,10 +1,20 @@
 package edu.api.bookflow.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import edu.api.bookflow.Model.UsuarioSistema;
 
+import java.util.List;
+
 @Repository
 public interface UsusarioSistemaRepository extends JpaRepository<UsuarioSistema, Long>{
+
+    @Query("SELECT us FROM UsuarioSistema us WHERE us.status = ?1")
+    Page<UsuarioSistema> findAllByStatus(@Param("status") Boolean status, Pageable pageable);
 
 }
