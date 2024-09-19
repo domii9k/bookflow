a7987e7bce9defff67059e8a2819f8b7f27f8e48
Dominique Ferreira <siqdominique99@gmail.com>
2024-09-19 15:29:16 -0300

    adiconando novas querys para validacao de usuario

--- src/main/java/edu/api/bookflow/Repository/UsusarioSistemaRepository.java
+++ src/main/java/edu/api/bookflow/Repository/UsusarioSistemaRepository.java
@@ -1,20 +1,22 @@
 package edu.api.bookflow.Repository;
 
+import edu.api.bookflow.Model.Usuario;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import edu.api.bookflow.Model.UsuarioSistema;
 
 import java.util.List;
 
 @Repository
-public interface UsusarioSistemaRepository extends JpaRepository<UsuarioSistema, Long>{
+public interface UsusarioSistemaRepository extends JpaRepository<Usuario, Long>{
 
-    @Query("SELECT us FROM UsuarioSistema us WHERE us.status = ?1")
-    Page<UsuarioSistema> findAllByStatus(@Param("status") Boolean status, Pageable pageable);
+    @Query("SELECT us FROM Usuario us WHERE us.status = ?1")
+    Page<Usuario> findAllByStatus(@Param("status") Boolean status, Pageable pageable);
+
+    List<Usuario> findByEmail(String email);
+    List<Usuario> findByCpf(String cpf);
 
 }
