2f76703bc354296a02ddfa43714abc208082fda6
Dominique Ferreira <siqdominique99@gmail.com>
2024-09-19 15:26:21 -0300

    adicionando classe GlobalPatch para requisições PATCH

--- src/main/java/edu/api/bookflow/Services/patchHttpRequest/GlobalPatch.java
+++ src/main/java/edu/api/bookflow/Services/patchHttpRequest/GlobalPatch.java
@@ -1,12 +1,21 @@
 package edu.api.bookflow.Services.patchHttpRequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 @Component
 public class GlobalPatch {
 
+    @Deprecated
+    /*
+    * Este método não permite que campos que nao foram preenchidos sejam nulos,
+    * obrigando que, nas classes DTO, os atributos tenham que permitir valores nulos,
+    * para, assim, realizar a atualização dos dados
+    * */
     //metodo de reflexo para implementação da requisição http patch
     public static <T> void patch(T entidadeExistente, T entidadeIncompleta) throws IllegalAccessException {
         Class<?> entidadeClass = entidadeExistente.getClass();//instancia a classe compilada
@@ -25,5 +34,19 @@ public class GlobalPatch {
         }
     }
 
+    public static <T> void globalPatch(Map<String, Object> fields, T classe){
+        ObjectMapper objectMapper = new ObjectMapper();
+        T classeConverte = objectMapper.convertValue(fields, (Class<T>) classe.getClass());
+        fields.forEach((chave, valor)-> {
+            Field field = ReflectionUtils.findField(classe.getClass(), chave);
+            field.setAccessible(true);
+
+            Object novoObjeto =  ReflectionUtils.getField(field, classeConverte);
+
+            ReflectionUtils.setField(field, classe, novoObjeto);
+
+            field.setAccessible(false);
+        });
+    }
 
 }
