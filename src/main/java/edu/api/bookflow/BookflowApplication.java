932e6553c57f0345189d3b0908a660a9acffb343
domii9k <110205632+domii9k@users.noreply.github.com>
2026-06-15 23:27:08 -0300

    Update BookflowApplication.java

--- src/main/java/edu/api/bookflow/BookflowApplication.java
+++ src/main/java/edu/api/bookflow/BookflowApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.dir}/.env")
+@PropertySource("file:${user.dir}/.env", ignoreResourceNotFound = true)
 public class BookflowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookflowApplication.class, args);
